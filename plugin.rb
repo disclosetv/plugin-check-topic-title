163cd791281a4a63e6b30cbd6005fb5eccb83d17
terrapop <T7Edo4tco41H>
2020-09-29 18:50:58 +0200

    First and final commit the second.

--- plugin.rb
+++ plugin.rb
@@ -10,8 +10,6 @@ after_initialize do
       DiscourseEvent.on(:after_validate_topic) do |topic|
         begin
           if (SiteSetting.plugin_check_topic_title_enabled)
-            Rails.logger.info("validate topic errors A: #{topic.to_json}")
-            Rails.logger.info("validate topic errors B: #{self.to_json}")
             if topic.title =~ /(http|www)/i
                 topic.errors.add("Title", " must not contain a link or URL.")
             end
