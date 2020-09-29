3c1d407ef86d5263828131072273950a8b7b76a3
terrapop <T7Edo4tco41H>
2020-09-29 18:45:53 +0200

    First and final commit.

---
+++ plugin.rb
@@ -0,0 +1,25 @@
+# name: plugin-check-topic-title
+# about:  Checks topic title for various things
+# version: 0.0.1
+# authors: Terrapop
+
+enabled_site_setting :plugin_check_topic_title_enabled
+
+after_initialize do
+
+      DiscourseEvent.on(:after_validate_topic) do |topic|
+        begin
+          if (SiteSetting.plugin_check_topic_title_enabled)
+            Rails.logger.info("validate topic errors A: #{topic.to_json}")
+            Rails.logger.info("validate topic errors B: #{self.to_json}")
+            if topic.title =~ /(http|www)/i
+                topic.errors.add("Title", " must not contain a link or URL.")
+            end
+          end
+        rescue => ex
+          Rails.logger.error(ex.message)
+          Rails.logger.error(ex.backtrace)
+        end
+      end
+
+end
